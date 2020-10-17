00e69ce80d63e5c690eade2ba31ecafdd5308563
fitri.manurung <fitri.manurung@go-jek.com>
2020-10-17 15:26:31 +0700

    Revert "complete assignment"
    This reverts commit 099bb4fed38bafead45fb2eedede481566382cf5.

--- cachematrix.R
+++ cachematrix.R
@@ -1,46 +1,15 @@
-## makeCacheMatrix and cacheSolve together provide functionality to calculate 
-## and cache the result of a inverse operation of a Matrix
+## Put comments here that give an overall description of what your
+## functions do
 
-## takes a matrix as input, provides getter/setter methods for contents and
-## inverse
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set = set, get = get, setinverse = setinverse,
-       getinverse = getinverse)
+
 }
 
-## The function, cacheSolve(x), calculates and caches the inverse of
-## a Matrix, x,  created via the function, makeCacheMatrix, shown above. 
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached inverse")
-    return(inv)
-  }
-  
-  data <- x$get()
-  inv <- solve(data)
-  x$setinverse(inv)
-  inv
+        ## Return a matrix that is the inverse of 'x'
 }
-
-# # Creates a matrix
-# myMatrix <- matrix(1:4, 2, 2)
-# # Creates a cacheable representation of myMatrix
-# cacheableMatrix <- makeCacheMatrix(myMatrix)
-# # Calculates and returns inverse of myMatrix and cache result
-# inverse1 <- cacheSolve(cacheableMatrix)
-# inverse1
-# # Returns inverse of myMatrix from cache
-# inverse2 <- cacheSolve(cacheableMatrix)
-# inverse2
